661e8779c4f8f06734af0ee70f30134d97507309
Andreas Pizsa <andreas.pizsa@oebb.at>
2016-02-04 10:36:56 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+/*!
+ * promise-spread <https://github.com/AndreasPizsa/promise-spread>
+ *
+ * Based on code by Benjamin Gruenbaum <https://github.com/benjamingr>
+ * http://stackoverflow.com/a/22776850/199263
+ *
+ * Copyright (c) 2016, Andreas Pizsa.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+function spread(promise) {
+  if(!promise) promise = Promise;
+  if (promise.prototype.spread) return;
+
+  promise.prototype.spread = function (fn) {
+    return this.then(function (args) {
+      return Promise.all(args);
+    }).then(function(args){
+      return fn.apply(this, args);
+    });
+  };
+}
+
+if(Promise) spread();
+
+module.exports.spread = spread;
---
+++ test.js
@@ -0,0 +1,16 @@
+var assert = require('assert');
+describe("Promise.spread", function(){
+  require('.');
+
+  it("can spread()", function(){
+    return Promise
+      .resolve([1,2,3,4])
+      .spread(function(a,b,c,d){
+        assert.strictEqual(arguments.length,4);
+        assert.strictEqual(a,1);
+        assert.strictEqual(b,2);
+        assert.strictEqual(c,3);
+        assert.strictEqual(d,4);
+      });
+  });
+});
